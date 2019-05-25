65e11b8e0348b4c1ede2e1ff0307d1580e664c71
drproduck <duckhiem95@yahoo.com>
2019-05-24 18:50:18 -0700

    oop style

--- beacon.py
+++ beacon.py
@@ -11,52 +11,120 @@ class Validator(object):
   def __repr__(self):
     return str(self.name)
 
+
 class Faction(object):
+
   def __init__(self, num, name):
+
     """Construct a Faction of validators that follow a common strategy
   
     Args:
       num: the number of validators in this faction
       name: the name of the faction
-      timings: function that decides the time to publish attestation
-      attestation_strat: function that decides which block to attest
       validators: the $(num) Validators
-
 
     """
     self.num = num
     self.name = name
-    self.timings = None
     # should return a list of (validator, slot) pairs
-    self.attestation_strat = None
     self.validators = {Validator(str("%s_%d") % (name, k), self) for k in range(num)}
 
-  @classmethod
-  def HonestFaction(cls, num, name, aim, error_param, delay_param):
-    """An faction of "honest" validators.
+  def get_timings(self):
+    """timings: function that decides the time to publish attestation"""
+    pass
+
+
+  def get_vote(self):
+    """return a vote (of a validator)"""
+    pass
+
+
+  def timing_with_mean(self, error_param, mean):
+    """timing with mean:
+    attests around 0.5 with error bar uniformly distributed within error_param"""
+    timings = []
+    for v in self.validators:
+      timing = adjusted_random_time(mean - error_param, mean + error_param)
+      timings.append((v, timing))
+    return timings
+
+
+
+
+class HonestFaction(Faction):
+
+  def __init__(self, num, name, aim, error_param, delay_param):
+    """A faction of "honest" validators.
     They are supposed to publish at time = aim with some noise
 
     """
-    f = cls(num, name)
-    f.timings = timing_with_mean(f, error_param, aim)
-    f.attestation_strat = (lambda v, t, f, vts:
-                           attestation_honest_majority(v, t, f, vts, delay_param, flipped=False))
-    return f
+    super().__init__(num, name)
+    self.aim = aim
+    self.error_param = error_param
+    self.delay_param = delay_param
+
+
+  def get_timings(self):
+    return self.timing_with_mean(self.error_param, self.aim)
+
+
+  def get_vote(self, time_current, votes, flipped=False):
+
+    """
+    Args:
+      time_current: the current time. Validators only see votes that come before this time.
+      votes: all the votes
+      delay_param: the validator receives vote at the published time + some unique delay for each vote
+      flipped: 
+      
+    Returns:
+      vote: the vote of this validator after considering all seeable votes
+
+    """
+    seen_votes = [0, 0]
+    for v, timing, vote in votes:
+      time_received = adjusted_random_time(timing, timing + self.delay_param)
+      logstr = "  %s voted for %s [t=%.3f; t_received=%.3f]" % (v.name, str(vote), timing, time_received)
+      if time_received < time_current:
+        logstr += (" (counts)")
+        seen_votes[vote] += 1
+      logger(logtoggle, logstr)
+    if seen_votes[0] >= seen_votes[1]:
+      vote = 0
+    else:
+      vote = 1
+    if flipped:
+      vote = 1 - vote
+    return vote
+
+
     
-  @classmethod
-  def SmokeFaction(cls, num, name, aim, error_param, delay_param):
+class SmokeFaction(Faction):
+
+  def __init__(self, num, name, aim, error_param, delay_param):
+
     """A faction of validators that publish votes randomly, at time approximately aim - delay_param,
     so that (honest) validators receive these votes at time approximately aim
     """
-    f = cls(num, name)
-    f.timings = timing_with_mean(f, error_param, aim-delay_param)
-    f.attestation_strat = (lambda v, t, f, vts:
-                           attestation_smokescreen())
-    return f
+
+    super().__init__(num, name)
+    self.aim = aim
+    self.error_param = error_param
+    self.delay_param = delay_param
+    self.timings = self.timing_with_mean(error_param, aim-delay_param)
+
+
+  def get_timings(self):
+    return self.timing_with_mean(self.error_param, self.aim - self.delay_param)
     
+
+  def get_vote(self, time, votes):
+    """smoke screen: vote randomly"""
+    return random.choice([0,1])
+
+
 def adjusted_random_time(lower, upper):
   """Uniformly random 
-  TODO: The current implementation assumes slot time of 1 second. Change this?
 
   """
   timing = random.uniform(lower, upper)
@@ -65,69 +133,6 @@ def adjusted_random_time(lower, upper):
   if timing > 0.99999:
     timing = 0.99999
   return timing
-
-def timing_with_mean(faction, error_param, mean=0.5):
-  """attests around 0.5 with error bar uniformly distributed within error_param"""
-  timings = []
-  for v in faction.validators:
-    timing = adjusted_random_time(mean - error_param, mean + error_param)
-    timings.append((v, timing))
-  return timings
-
-def attestation_honest_majority(validator, time_current, factions, votes, delay_param, flipped=False):
-  """The honest way of things
-  Args:
-    validator: ?
-    time_current: the current time. Validators only see votes that come before this time.
-    factions: ?
-    votes: all the votes
-    delay_param: the validator receives vote at the published time + some unique delay for each vote
-    flipped: ?
-    
-  Returns:
-    vote: the vote of this validator after considering all seeable votes
-
-  """
-  seen_votes = [0, 0]
-  for (v, timing, vote) in votes:
-    time_received = adjusted_random_time(timing, timing + delay_param)
-    logstr = "  %s voted for %s [t=%.3f; t_received=%.3f]" % (v.name, str(vote), timing, time_received)
-    if (time_received < time_current):
-      logstr += (" (counts)")
-      seen_votes[vote] += 1
-    logger(logtoggle, logstr)
-  if seen_votes[0] >= seen_votes[1]:
-    vote = 0
-  else:
-    vote = 1
-  if flipped:
-    vote = 1 - vote
-  return vote
-
-def attestation_smokescreen():
-  """Smokescreen strategy: vote randomly
-  """
-  return random.choice([0,1])
-  
-def play(faction1, faction2):
-  factions = (faction1, faction2)
-  timings = []
-  for f in factions:
-    timings += f.timings
-  timings.sort(key = lambda x: x[1])
-  votes = []
-  for t in timings:
-    v = t[0]
-    time = t[1]
-    f = v.faction
-    logger(logtoggle, "%s votes [t=%.3f]" % (v.name, time))
-    vote = f.attestation_strat(v, time, factions, votes)
- #    v.vote = vote
- #   v.vote_time = t[1]
-    logger(logtoggle, "%s votes %s" % (v.name, str(vote)))
-
-    votes.append((v, time, vote))    
-  return votes
 
 def logger(toggle, log): #logging helper function
   if (toggle == 0):
--- main.py
+++ main.py
@@ -1,40 +1,65 @@
+#!/usr/env/bin python
+
 import matplotlib.pyplot as plt
 from collections import Counter
 import logging
-import beacon
+from beacon import *
 
 # logging = 0 # uncomment to turn off logging
 
+def play(faction1, faction2):
+  factions = (faction1, faction2)
+  timings = []
+  for f in factions:
+    timings += f.get_timings()
+  timings.sort(key = lambda x: x[1])
+  votes = []
+  for t in timings:
+    v = t[0]
+    time = t[1]
+    logger(logtoggle, "%s votes [t=%.3f]" % (v.name, time))
+    vote = v.faction.get_vote(time, votes)
+    logger(logtoggle, "%s votes %s" % (v.name, str(vote)))
+    votes.append((v, time, vote))    
+  return votes
+
 
 def collect_statistics(votes):
-    vote_count = []
-    for vote in votes:
-        vote_count += [vote[2]]
+  vote_count = []
+  for vote in votes:
+      vote_count += [vote[2]]
 
-    vote_count = Counter(vote_count)
+  vote_count = Counter(vote_count)
 
-    return (vote_count[0], vote_count[1])
+  return (vote_count[0], vote_count[1])
 
-######
-def main(num, honestFac,dishonestFac, aim, error_param, delay_param, n_simulations):
-    
-    if __name__ == "__main__": # log with the following format
-        logging.basicConfig(level=logging.DEBUG, filename="logfile", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-
-    # factions
-    faction1 = beacon.Faction.HonestFaction(num, honestFac, aim, error_param, delay_param)
-    faction2 = beacon.Faction.SmokeFaction(num, dishonestFac, aim, error_param, delay_param)
-
-    # number of blocks reaching 2/3 consensus
-    # to do: more here
-    stats = [collect_statistics(beacon.play(faction1, faction2)) for _ in range(n_simulations) ] 
-    
-    n = 0
-    for s in stats:
-        if s[0] / s[1] >= 2/3 or s[1] / s[0] >= 2/3:
-            n += 1
 
     
-######
-main(20, 'honest','smoke', 0.5, 0.1, 0.3, 100) 
+if __name__ == "__main__": # log with the following format
+  num = 20
+  honestFac = 'honest'
+  dishonestFac = 'smoke'
+  aim = 0.5
+  error_param = 0.1
+  delay_param = 0.2
+  n_simulations = 100
+  logging.basicConfig(level=logging.DEBUG, filename="logfile", filemode="a+",
+                  format="%(asctime)-15s %(levelname)-8s %(message)s")
+
+  # factions
+  faction1 = HonestFaction(num, honestFac, aim, error_param, delay_param)
+  faction2 = SmokeFaction(num, dishonestFac, aim, error_param, delay_param)
+
+  # number of blocks reaching 2/3 consensus
+  # to do: more here
+  stats = [collect_statistics(play(faction1, faction2)) for _ in range(n_simulations) ] 
+  
+  print(stats)
+  n = 0
+  for s in stats:
+    if s[0] / s[1] >= 3/2 or s[0] / s[1] <= 2/3:
+      n += 1
+
+  print('win/lose ration: %d\n' % n)
+
+  
