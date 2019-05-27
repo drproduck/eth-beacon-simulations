7e17ca4e2fe53f702c4dad63ac0b2b4f5dc8a248
drproduck <duckhiem95@yahoo.com>
2019-05-26 18:23:08 -0700

    uniform creation of dynamic sets

--- dynamic.py
+++ dynamic.py
@@ -2,62 +2,55 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-n = 1000000
-#Master list of activators and exits
-M = []
-#Master list of Bounds
-B = []
-#Master list of intersections
-I = []
-for i in range(n):
-    ###Partitions initial validator set into four parts
-    #L_0 = [A,B,C,D]
+n_simulations = 100
 
-    'Can probably rewrite this as a function'
-    #Samples four uniform numbers
-    Numbers = np.random.rand(4)
-    #Normalizes so they add up to 1 to make them percentages
-    L_0 = [i/sum(Numbers) for i in Numbers]
-    #L_0 = [A,B,C,D]
+# in this version, the sizes of 3 sets are allowed to be different.
 
-    ###The rest of V_L (percentage-wise) is based on information from initial validator set
-    #V_L = [B,C,E,F]
-    #1 - (B + C) = E + F
-    x = 1 - (L_0[1] + L_0[2])
+def sample_fixed_sum(sigma, n):
+    """sample n integers which sum to sigma"""
+    res = [np.random.randint(0, sigma)]
+    part_sum = res[0]
+    for i in range(1,n-1):
+        res.append(np.random.randint(0, sigma - part_sum))
+        part_sum += res[-1]
+    res.append(sigma - part_sum)
+    return res
 
-    ##Constructs F explicitly
-    'Can probably rewrite this as a function'
-    #Samples two uniform numbers within the range of activations of L
-    X = np.array([random.uniform(0,x), random.uniform(0,x)])
-    #Normalizes this so they add up to x
-		Normalized_X = x * (X / sum(X))
-    ##sum(Normalized_X) - x should be machine 0
-    #Normalized_X = [E,F]
+def sample_sets(size_0=20, size_L=40, size_R=60):
+    """does this algorithm admit unbiased distribution (each possible config has same probability)?"""
+    # according to figure 10
+    # first sample C C = np.random.randint(0, min(size_0, size_L, size_R))
+    # then sample B,D,F
+    B = np.random.randint(0, min(size_0 - C, size_L - C))
+    D = np.random.randint(0, min(size_0 - B - C, size_R - C))
+    F = np.random.randint(0, min(size_L - B - C, size_R - D - C))
+    # then compute A, E, G
+    A = size_0 - B - C - D
+    E = size_L - B - C - F
+    G = size_R - C - D - F
 
-    #V_R = [C,D,F,G]
-    #1 - (C + D + F) = G, so
-    #1 - (C + D) = F + G
+    return A,B,C,D,E,F,G
 
-    ###Assigns the activators and exits
-    #a_L = E + F
-    a_L = x
-    #e_L = A + D
-    e_L = L_0[0]+L_0[3]
 
-    #a_R = F+ G
-    a_R = 1 - (L_0[2]+L_0[3])
-    #e_R = A + B
-    e_R = L_0[0] + L_0[1]
 
-    #lower bound
-    bound = 2*a_L/3 + 2*e_R/3 + a_R/3 + e_L/3
-    #intersection = C+F
-    intersection = L_0[2] + Normalized_X[1]
+for i in range(n_simulations):
+    size_0 = 892
+    size_L = 892
+    size_R = 892
 
-    #If they're within the bound C + F \ge |V_L|/3 - (2a_L/3 + 2e_R/3 + a_R/3 + e_L/3\right)
-    'Problem: Size of V_L is not 1 -- but that might not matter because these are percentages'
-    if intersection >= 1/3 - bound:
-        #Append to the master list
-        M.append([a_L,e_L,a_R,e_R])
-        B.append(bound)
-        I.append(intersection)
+    A,B,C,D,E,F,G = sample_sets(size_0, size_L, size_R)
+    eL = A + D
+    aL = E + F
+    eR = A + B
+    aR = F + G
+
+    bigunion = A+B+C+D+E+F+G
+    bound102 = 2/3*size_L + 2/3*size_R - bigunion
+    bound103 = size_L/3 - (2/3*aL + 2/3*eR + aR/3 + eL/3)
+    print(bound102, bound103)
+
+    # lemma 10.2: if B_L and B_R are finalized, is this necessary that the intersection of their attestation sets > 0?
+    
+
+
+
